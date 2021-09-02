3c16d0b353fbc46f56aa0a94b1ca1dc68db1dfb8
Callum <80120239+cmr33-cloud@users.noreply.github.com>
2021-09-02 12:26:56 +0100

    new

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-head.addEventListener("click", () => {body.style = "background-color: red"});
+head.addEventListener("click", () => {body.style.backgroundColor = "red"});
+head.addEventListener("mouseout", () => {body.style.backgroundColor = "white"});
+body.addEventListener("keydown", (e) =>  {
+    para = document.createElement('p');
+    para.textContent = e.key;
+    body.appendChild(para);
+});
+let wiki = document.querySelector('h2');
+wiki.addEventListener('mouseover', () => {wiki.textContent = "Welcome to my world!"});
+function highlight(object){if(object.style.fontWeight != "bold"){object.style.fontWeight = "bold"}
+else{object.style.fontWeight = "normal"};
+if(object.style.fontSize=="20px"){object.style.fontSize="30px"}
+else {object.style.fontSize="20px"}
+};
+// document.querySelector('p').style.fontSize = "40px";
+earlypara.style.fontSize = "20px", careerpara.style.fontSize = "20px", personalpara.style.fontSize = "20px", 
+// document.querySelectorAll('li').addEventListener('click', (e) => {
+//     console.log(e)
+//     // highlight(e)
+// });
+
+early.addEventListener('click', () => {highlight(earlypara)});
+ career.addEventListener('click', () => {highlight(careerpara)});
+personal.addEventListener('click', () => {highlight(personalpara)});
